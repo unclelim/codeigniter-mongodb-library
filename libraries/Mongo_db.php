9cb9e4b7855502445f64184e172dc3f70df1e02b
Alex Bilbie <alex@alexbilbie.com>
2012-01-31 17:41:08 +0000

    Add to set spelling fix. Fixes #35

--- libraries/Mongo_db.php
+++ libraries/Mongo_db.php
@@ -1247,7 +1247,7 @@ class Mongo_db {
 		
 		elseif (is_array($values))
 		{
-			$this->updates['$attToSet'][$field] = array('$each' => $values);
+			$this->updates['$addToSet'][$field] = array('$each' => $values);
 		}
 		
 		return $this;
